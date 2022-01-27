4f19f8af20785c22f748d01e474edc77d4c958bb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-01-27 15:50:04 -0500

    check scraped

--- README.md
+++ README.md
@@ -8,4 +8,8 @@ To see all Cypress blog post URLs
 
 ```
 $ npm run demo:list-posts
+# saved blog-post-urls.json with the list of urls
+$ as-a . node ./filter-scraped-posts
+# reads the blog-post-urls.json, checks which posts were scraped
+# and saved the remaining urls into the file need-scraping.json
 ```
---
+++ filter-scraped-posts.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const allPosts = require('./blog-post-urls.json').slice(0, 3)
+console.log('checking %d posts if they are scraped', allPosts.length)
+
+const { markScraped, wasScraped } = require('was-it-scraped')
+
+async function checkScrapeStatus(urls) {
+  const results = []
+  for (let url of urls) {
+    const scraped = await wasScraped(url)
+    if (!scraped) {
+      results.push(url)
+    }
+  }
+
+  return results
+}
+
+checkScrapeStatus(allPosts).then((list) => {
+  const filename = 'need-scraping.json'
+  fs.writeFileSync(filename, JSON.stringify(list, null, 2) + '\n')
+  console.log('saved %d links to be scraped into %s', list.length, filename)
+})
