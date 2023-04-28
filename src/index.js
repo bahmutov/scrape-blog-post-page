00920642b5e995a884bdb88b6b1b876d8919a18e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-04-28 10:13:44 -0400

    feat: export getBlogPostUrls function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const { getModifiedPostUrls } = require('./get-modified-post-urls')
+const { getBlogPostUrls } = require('./get-post-urls')
 
-module.exports = { getModifiedPostUrls }
+module.exports = { getModifiedPostUrls, getBlogPostUrls }
